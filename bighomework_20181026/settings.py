00a4fb9448856cb6f84ba4d252a58b7756a16541
xucrt <xuchuanru@gmail.com>
2018-10-31 12:09:41 +0900

    settings.py update Error with mysql component mysqlclient. Change database from mysql to sqlite3.

--- bighomework_20181026/settings.py
+++ bighomework_20181026/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'bighomework_20181026.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'xucrt.mysql.pythonanywhere-services.com',
-        'NAME': 'xucrt$big_homework_DB',
-        'USER': 'xucrt',
-        'PASSWORD': '0987poiu',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'HOST': 'xucrt.mysql.pythonanywhere-services.com',
+        #'NAME': 'xucrt$big_homework_DB',
+        #'USER': 'xucrt',
+        #'PASSWORD': '0987poiu',
     }
 }
 
